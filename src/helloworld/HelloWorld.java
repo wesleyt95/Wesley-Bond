dc205455328c3afa3d69e9398406b2e42625308b
wesleybond <wesleybond@10.175.119.95>
2016-09-19 12:44:38 -0400

    First Commit

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,16 +1,38 @@
-package helloworld;
-
-/**
- *
- * @author wesleybond
- */
-public class HelloWorld {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+/*
+* Comments Example Program
+* Wesley Bond
+* July 21st, 2015
+*/
+import java.util.Random;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        // New Random Number
+        Random random = new Random();
+        long from = 1;
+        long to = 100;
+        int randomNumber = random.nextInt + from;
+        // Guess the number
+        int guessedNumber = 0;
+ 
+        // Print out the revealed number
+        System.out.printf("The number is between %d and %d.\n", from, to);
+        
+        // Print sequence
+        do
+        {
+            System.out.print("Guess what the number is: ");
+            guessedNumber = scan.nextInt();
+            if (guessedNumber > randomNumber)
+                System.out.println("Your guess is too high!");
+            else if (guessedNumber < randomNumber)
+                System.out.println("Your guess is too low!");
+            else
+                System.out.println("You got it!");
+        } while (guessedNumber != randomNumber);
     }
-    
-}
+} 
